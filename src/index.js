f7ffd88e343d2ee5f9f393344ee4a7f18acf242f
kourosh <kourosh9bhl@gmail.com>
2023-10-13 10:11:53 +0330

    implement star rating

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import Star from './Star';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode>{/* <App /> */ <Star starLength={10} />}</React.StrictMode>
 );
